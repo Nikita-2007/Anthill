f187bbd546f8dc86c9d3490bc76a4cd2ff0077f1
Nikita-2007 <nikita2007ua@gmail.com>
2022-03-19 20:02:27 +0300

    рандомное вращение

--- JS/Ant.js
+++ JS/Ant.js
@@ -2,30 +2,21 @@
 
 class Ant {
     constructor() {
+        this.x = Math.round(Math.random()*window.innerWidth);
+        this.y = Math.round(Math.random()*window.innerHeight);
+        this.ang = Math.round(Math.random()*Math.PI*2);
+        this.col = ('#'+Math.floor(Math.random()*16777216).toString(16).padStart(6, '0'));
         this.pose = false;
-        this.size = 1;
     }
 
-    draw(x, y, ang, col) {
-        
+    draw(fw) {
+        let x = this.x;
+        let y = this.y;
+        let ang = this.ang;
+        let col = this.col;
         //Данные для расчёта
         let ctx = view.ctx;
         this.pose = !this.pose;
-        let size5 = this.size;
-        let size10 = this.size*2;
-        let size15 = this.size*3;
-        let size18 = this.size*3.6;
-        let size20 = this.size*4;
-        let size25 = this.size*5.6;
-        let size28 = this.size*5.6;
-        let size30 = this.size*6;
-        let size32 = this.size*6.4;
-        let size40 = this.size*8;
-        let size45 = this.size*9;
-        let size50 = this.size*10;
-        let size55 = this.size*11;
-        let size65 = this.size*13;
-        let size100 = this.size*20;
 
         ctx.lineWidth = 1.5;
         ctx.strokeStyle='rgb(32, 16, 8)';
@@ -34,64 +25,86 @@ class Ant {
         //Поворот
         ctx.save();
         ctx.translate(x, y);
-        ctx.rotate(ang);
+        ctx.rotate(ang*Math.random()*2);
         ctx.translate(-x, -y);
 
         ctx.beginPath();
 
         //Ноги левые
-        ctx.moveTo(x, y+size25);
-        ctx.lineTo(x+size30, y);
-        ctx.lineTo(x+size40, y-size20);
+        ctx.moveTo(x, y+fw.size25);
+        ctx.lineTo(x+fw.size30, y);
+        ctx.lineTo(x+fw.size40, y-fw.size20);
 
-        ctx.moveTo(x, y+size28);
-        ctx.lineTo(x+size30, y+size28);
-        ctx.lineTo(x+size50, y+size45);
+        ctx.moveTo(x, y+fw.size28);
+        ctx.lineTo(x+fw.size30, y+fw.size28);
+        ctx.lineTo(x+fw.size50, y+fw.size45);
 
-        ctx.moveTo(x, y+size32);
-        ctx.lineTo(x+size30, y+size55);
-        ctx.lineTo(x+size40, y+size100);
+        ctx.moveTo(x, y+fw.size32);
+        ctx.lineTo(x+fw.size30, y+fw.size55);
+        ctx.lineTo(x+fw.size40, y+fw.size100);
 
         //Ноги правые
-        ctx.moveTo(x, y+size25);
-        ctx.lineTo(x-size30, y);
-        ctx.lineTo(x-size40, y-size20);
+        ctx.moveTo(x, y+fw.size25);
+        ctx.lineTo(x-fw.size30, y);
+        ctx.lineTo(x-fw.size40, y-fw.size20);
 
-        ctx.moveTo(x, y+size28);
-        ctx.lineTo(x-size30, y+size28);
-        ctx.lineTo(x-size50, y+size45);
+        ctx.moveTo(x, y+fw.size28);
+        ctx.lineTo(x-fw.size30, y+fw.size28);
+        ctx.lineTo(x-fw.size50, y+fw.size45);
 
-        ctx.moveTo(x, y+size32);
-        ctx.lineTo(x-size30, y+size55);
-        ctx.lineTo(x-size40, y+size100);
+        ctx.moveTo(x, y+fw.size32);
+        ctx.lineTo(x-fw.size30, y+fw.size55);
+        ctx.lineTo(x-fw.size40, y+fw.size100);
 
         ctx.stroke();
         ctx.closePath();
 
         //Телo
         ctx.beginPath();
-        ctx.ellipse(x, y, size10, size10, 0, 0, Math.PI*2);
+        ctx.ellipse(x, y, fw.size10, fw.size10, 0, 0, Math.PI*2);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
         ctx.beginPath();
-        ctx.ellipse(x, y+size28, size10, size18, 0, 0, Math.PI*2);
+        ctx.ellipse(x, y+fw.size28, fw.size10, fw.size18, 0, 0, Math.PI*2);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
         ctx.beginPath();
-        ctx.ellipse(x, y+size65, size15, size20, 0, 0, Math.PI*2);
+        ctx.ellipse(x, y+fw.size65, fw.size15, fw.size20, 0, 0, Math.PI*2);
         
         //Усики
-        ctx.moveTo(x-size5, y-size5);
-        ctx.lineTo(x-size15 + this.pose, y-size30);
+        ctx.moveTo(x-fw.size5, y-fw.size5);
+        ctx.lineTo(x-fw.size15 + this.pose, y-fw.size30);
 
-        ctx.moveTo(x+size5, y-size5);
-        ctx.lineTo(x+size15 - this.pose, y-size30);
+        ctx.moveTo(x+fw.size5, y-fw.size5);
+        ctx.lineTo(x+fw.size15 - this.pose, y-fw.size30);
         
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
         ctx.restore()
+    }
+}
+
+class FlyWeidth {
+    constructor() {
+        this.size = 1;
+        this.size5 = this.size;
+        this.size10 = this.size*2;
+        this.size15 = this.size*3;
+        this.size18 = this.size*3.6;
+        this.size20 = this.size*4;
+        this.size25 = this.size*5.6;
+        this.size28 = this.size*5.6;
+        this.size30 = this.size*6;
+        this.size32 = this.size*6.4;
+        this.size40 = this.size*8;
+        this.size45 = this.size*9;
+        this.size50 = this.size*10;
+        this.size55 = this.size*11;
+        this.size65 = this.size*13;
+        this.size100 = this.size*20;
+
     }
 }
--- JS/View.js
+++ JS/View.js
@@ -6,11 +6,14 @@ class View {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext('2d');
+        this.fw = new FlyWeidth();
     }
 
     draw() {
         this.ctx.fillStyle = 'darkslategray';
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        model.ant.draw(300, 300, 1.0, 'red');
+        for (let ant of model.listAnt) {
+            ant.draw(this.fw);
+        }
     }
 }
